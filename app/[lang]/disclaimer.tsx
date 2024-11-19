f62242b805001df0388a2d5def44ca8d443d577f
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-11-19 11:39:16 +0100

    Initial version

---
+++ app/[lang]/disclaimer.tsx
@@ -0,0 +1,27 @@
+import { UNIT } from '@/lib/dictionary';
+import { Language, type Translation } from '@/locales';
+import { Alert, BodyShort } from '@navikt/ds-react';
+
+interface DisclaimerProps {
+  lang: Language;
+  className?: string;
+}
+
+export const Disclaimer = ({ lang, className }: DisclaimerProps) => (
+  <Alert variant="info" className={className}>
+    <BodyShort spacing>{FIRST_LINE[lang]}</BodyShort>
+    <BodyShort>{SECOND_LINE[lang]}</BodyShort>
+  </Alert>
+);
+
+const FIRST_LINE: Translation = {
+  [Language.NB]: `Alle klager behandles av ${UNIT.vedtaksinstans.nb} før de vises her.`,
+  [Language.NN]: `Alle klager blir behandla av ${UNIT.vedtaksinstans.nn} før dei blir vist her.`,
+  [Language.EN]: `All complaints are processed by ${UNIT.vedtaksinstans.en} before they are shown here.`,
+};
+
+const SECOND_LINE: Translation = {
+  [Language.NB]: `Her ser du klager og anker som er hos ${UNIT.klageinstans.nb}. Du ser bare klager og anker for noen ytelser.`,
+  [Language.NN]: `Her ser du klager og anker som er hos ${UNIT.klageinstans.nn}. Du ser berre klager og anker for nokre ytingar.`,
+  [Language.EN]: `Here you can see complaints and appeals that are with ${UNIT.klageinstans.en}. You only see complaints and appeals for some services.`,
+};
