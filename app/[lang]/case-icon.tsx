00ef1d9ad2e96c676b1694283d21156366cc2f6d
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-01-30 10:57:12 +0100

    Initial version

---
+++ app/[lang]/case-icon.tsx
@@ -0,0 +1,20 @@
+import { CaseType } from '@/lib/types';
+import { ArrowCirclepathIcon, DocPencilIcon, ParagraphIcon } from '@navikt/aksel-icons';
+
+interface CaseIconProps {
+  typeId: CaseType;
+  className?: string;
+}
+
+export const CaseIcon = ({ typeId, ...rest }: CaseIconProps) => {
+  switch (typeId) {
+    case CaseType.KLAGE:
+      return <DocPencilIcon {...rest} aria-hidden />;
+    case CaseType.ANKE:
+      return <ParagraphIcon {...rest} aria-hidden />;
+    case CaseType.OMGJØRING:
+      return <ArrowCirclepathIcon {...rest} aria-hidden />;
+    default:
+      return null;
+  }
+};
---
+++ lib/get-language.ts
@@ -0,0 +1,36 @@
+import { LANGUAGE_HEADER } from '@/lib/custom-headers';
+import { DEFAULT_LANGUAGE, type Language, isLanguage } from '@/locales';
+import type { DecoratorLocale } from '@navikt/nav-dekoratoren-moduler/ssr';
+import type { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
+import { headers } from 'next/headers';
+
+interface Params {
+  lang: DecoratorLocale;
+}
+
+export const getLanguage = async (params: Promise<Params>): Promise<Language> => {
+  const { lang } = await params;
+
+  if (isLanguage(lang)) {
+    return lang;
+  }
+
+  return DEFAULT_LANGUAGE;
+};
+
+export const getDecoratorLanguage = async (): Promise<Language> => {
+  const headerList = await headers();
+  const decoratorLocale = headerList.get(LANGUAGE_HEADER);
+
+  if (decoratorLocale !== null && isLanguage(decoratorLocale)) {
+    return decoratorLocale;
+  }
+
+  return DEFAULT_LANGUAGE;
+};
+
+export const getLanguageFromHeaders = (headers: ReadonlyHeaders): Language => {
+  const langHeader = headers.get(LANGUAGE_HEADER);
+
+  return isLanguage(langHeader) ? langHeader : DEFAULT_LANGUAGE;
+};
