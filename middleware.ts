16a8e81e174aacac9a598aeed000e79eaac8f7cd
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-01-30 10:57:12 +0100

    Initial version

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { CURRENT_PATH_HEADER, DECORATOR_LANGUAGE_COOKIE, LANGUAGE_HEADER } from '@/lib/custom-headers';
+import { DEFAULT_LANGUAGE, isLanguage } from '@/locales';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const headers = new Headers();
+
+  headers.set(CURRENT_PATH_HEADER, request.nextUrl.pathname);
+
+  const lang = request.cookies.get(DECORATOR_LANGUAGE_COOKIE);
+
+  headers.set(LANGUAGE_HEADER, lang !== undefined && isLanguage(lang.value) ? lang.value : DEFAULT_LANGUAGE);
+
+  return NextResponse.next({ headers });
+}
