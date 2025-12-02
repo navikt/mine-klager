4e7becc2c4d26c9e641e7ece6d28017576731a4d
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-12-02 10:59:34 +0100

    Migrate middleware to proxy - NextJS change

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { type NextRequest, NextResponse } from 'next/server';
 import { CURRENT_PATH_HEADER, DECORATOR_LANGUAGE_COOKIE, LANGUAGE_HEADER } from '@/lib/server/custom-headers';
 import { DEFAULT_LANGUAGE, isLanguage } from '@/locales';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const headers = new Headers();
 
   headers.set(CURRENT_PATH_HEADER, request.nextUrl.pathname);
