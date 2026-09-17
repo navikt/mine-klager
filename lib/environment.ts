568f3ba9a148fa6da1ea71a4c41eea1feaf0c796
Christian Skrøvseth <3177301+cskrov@users.noreply.github.com>
2026-09-17 14:26:58 +0200

    Handle unsupported case types

--- lib/environment.ts
+++ lib/environment.ts
@@ -4,7 +4,7 @@ export const isClient = typeof window !== 'undefined';
 
 const NAIS_CLUSTER_NAME = isClient ? getClientEnv('data-environment') : process.env.NAIS_CLUSTER_NAME;
 
-const isDeployedToDev = NAIS_CLUSTER_NAME === 'dev-gcp';
+export const isDeployedToDev = NAIS_CLUSTER_NAME === 'dev-gcp';
 export const isDeployedToProd = NAIS_CLUSTER_NAME === 'prod-gcp';
 export const isDeployed = isDeployedToDev || isDeployedToProd;
 export const isLocal = !isDeployed;
--- lib/server/api.ts
+++ lib/server/api.ts
@@ -1,10 +1,10 @@
 import type { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
-import { isLocal } from '@/lib/environment';
+import { isDeployedToDev, isLocal } from '@/lib/environment';
 import { InternalServerError, UnauthorizedError } from '@/lib/errors';
 import { getLogger } from '@/lib/logger';
 import { getFromKabal } from '@/lib/server/fetch';
 import { getLanguageFromHeaders } from '@/lib/server/get-language';
-import type { GetSakerResponse, Sak } from '@/lib/types';
+import { type GetSakerResponse, isCaseType, type Sak } from '@/lib/types';
 import { Language, type Translation } from '@/locales';
 
 const logger = getLogger('api');
@@ -35,7 +35,30 @@ export const getSaker = async (headers: Headers): Promise<GetSakerResponse> => {
       throw new InternalServerError(res.status, FAILED_TO_FETCH[lang], lang);
     }
 
-    return res.json();
+    const { saker }: GetSakerResponse = await res.json();
+
+    const supportedSaker: Sak[] = [];
+    const unsupportedSaker: Sak[] = [];
+
+    for (const sak of saker) {
+      if (isCaseType(sak.typeId)) {
+        supportedSaker.push(sak);
+      } else {
+        unsupportedSaker.push(sak);
+      }
+    }
+
+    if (unsupportedSaker.length !== 0) {
+      const cases = unsupportedSaker.map(({ id, typeId }) => `Case ${id} of type ${typeId}`).join(', ');
+
+      if (isLocal || isDeployedToDev) {
+        logger.info('Unsupported cases found:', { cases });
+      } else {
+        logger.warn('Unsupported cases found:', { cases });
+      }
+    }
+
+    return { saker: supportedSaker };
   } catch (error) {
     if (error instanceof InternalServerError || error instanceof UnauthorizedError) {
       throw error;
@@ -53,9 +76,9 @@ export const getSaker = async (headers: Headers): Promise<GetSakerResponse> => {
 };
 
 export const getSak = async (headers: ReadonlyHeaders, id: string): Promise<Sak | undefined> => {
-  const response = await getSaker(headers);
+  const { saker } = await getSaker(headers);
 
-  return response.saker.find((sak) => sak.id === id);
+  return saker.find((sak) => sak.id === id);
 };
 
 const FAILED_TO_FETCH: Translation = {
